9f50b90635c887016ba1ded56992021566fb3713
nick powell <nickjaypowell@gmail.com>
2020-01-11 01:31:34 -0700

    neatness

--- src/user_input_list.rs
+++ src/user_input_list.rs
@@ -3,8 +3,8 @@ imp!();
 pub fn user_select_from_list <T> (things: &Vec<T>) -> Result<usize, Box<dyn Error>> where
     T: std::fmt::Debug
 {
-    for thing in things.iter().enumerate() {
-        println!("{:?}", thing);
+    for (i, thing) in things.iter().enumerate() {
+        println!("{}: {:?}", i, thing);
     }
     let mut buf = String::new();
     io::stdin().read_line(&mut buf)?;
