fd43312cb2a4088b52de62dea3f65cb28f062e38
nick powell <nickjaypowell@gmail.com>
2020-01-11 04:25:05 -0700

    quotes

--- src/win_app_search.rs
+++ src/win_app_search.rs
@@ -2,16 +2,18 @@ imp!();
 
 pub fn find_windows_app(app_name: &str) -> PathBuf {
     let di = std::string::String::from_utf8(
-        Command::new("powershell").arg(format!("Get-ItemProperty HKLM:\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\{} | Select-Object DisplayIcon", app_name)
+        Command::new("powershell").arg(format!(r#"Get-ItemProperty "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\\{}" | Select-Object DisplayIcon"#, app_name)
         ).output().unwrap().stdout
     ).unwrap();
-    let reg = Regex::new(r#""(?P<path>.*)""#).expect("bad regex");
-
+  
+    let reg = Regex::new(r#"\n(?P<path>.*),"#).expect("bad regex");
+  
     PathBuf::from(
         reg.captures(&di)
             .expect("capture failed")
             .name("path")
             .expect("path not found")
-            .as_str(),
+            .as_str()
+            .trim_matches('"')
     )
-}
+  }
