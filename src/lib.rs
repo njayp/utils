df6107ffe445756ae20c859c11950fbd98895ef2
nick powell <nickjaypowell@gmail.com>
2020-01-10 23:01:16 -0700

    try

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,22 @@
+#![allow(unused_imports)]
 
+#[macro_use]
+mod macros {
+    macro_rules! imp {
+        () => {
+            
+            use std::error::Error;
+            use std::io;
+            use std::path::PathBuf;
+            use std::process::Command;
+
+            //use lazy_static::lazy_static;
+            use regex::Regex;
+            //use serde::{Deserialize, Serialize};
+            //use soup::*;
+        };
+    }
+}
 
 pub use self::win_app_search::find_windows_app;
 mod win_app_search;
